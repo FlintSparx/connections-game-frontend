32f49593cb17fc736ec523b6b07a37fa023b85c3
nick <nickbernardi123@gmail.com>
2025-05-13 22:39:04 -0400

    Added word tile component and game board grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import GameBoard from './components/GameBoard'
+import './App.css'
 
 function App() {
   return (
-    <div>
-      <h1>Connections Game</h1>
-      {/* Game board and other components will go here */}
+    <div className="container mx-auto p-4 text-center">
+      <h1 className="text-2xl font-bold mb-4">Connections Game</h1>
+      <p className="text-sm text-gray-600 mb-4">
+        Find groups of four related words
+      </p>
+      
+      {/* added our game board component */}
+      <GameBoard />
+      
+      {/* todo: add other game controls and info later */}
+      <div className="mt-4">
+        <button className="bg-blue-500 text-white px-4 py-2 rounded mr-2">
+          Submit
+        </button>
+        <button className="bg-gray-300 px-4 py-2 rounded">
+          Shuffle
+        </button>
+      </div>
     </div>
   )
 }
--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import WordTile from './WordTile'
+
+// basic wireframe:
+// +-------+-------+-------+-------+
+// | Tile1 | Tile2 | Tile3 | Tile4 |
+// +-------+-------+-------+-------+
+// | Tile5 | Tile6 | Tile7 | Tile8 |
+// +-------+-------+-------+-------+
+// | Tile9 | Tile10| Tile11| Tile12|
+// +-------+-------+-------+-------+
+// | Tile13| Tile14| Tile15| Tile16|
+// +-------+-------+-------+-------+
 
 function GameBoard() {
+  // for now just using dummy data for testing
+  // todo: get real game data from api
+  const sampleWords = [
+    "Apple", "Banana", "Cherry", "Date", 
+    "Elephant", "Frog", "Giraffe", "Hippo", 
+    "Igloo", "Jacket", "Kite", "Lemon", 
+    "Mountain", "Notebook", "Orange", "Pencil"
+  ]
+  
   return (
-    <div>
-      {/* Game board will be displayed here */}
-      Game Board
+    <div className="max-w-md mx-auto p-4">
+      {/* game board container with grid layout */}
+      <div className="grid grid-cols-4 gap-2">
+        {/* mapping through sample words to create tiles */}
+        {sampleWords.map((word, index) => (
+          <WordTile key={index} word={word} />
+        ))}
+      </div>
     </div>
   )
 }
