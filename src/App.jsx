d0ff45bb3a8545a33ed98ed0fff4d8a919af89c3
FlintSparx <burritos642@gmail.com>
2025-05-19 20:48:49 -0400

    Registration form is complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import GameBoard from './components/GameBoard'
 import './App.css'
+import RegistrationForm from './components/RegistrationForm';
 
-function App() {
+function GamePage() {
   return (
     <div className="container mx-auto p-4 text-center">
       <h1 className="text-2xl font-bold mb-4">Connections Game</h1>
@@ -23,6 +25,31 @@ function App() {
         </button>
       </div>
     </div>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        {/* Simple navigation bar */}
+        <nav className="bg-blue-500 p-4 text-white">
+          <div className="container mx-auto flex justify-between">
+            <Link to="/" className="font-bold">Connections App</Link>
+            <div>
+              <Link to="/" className="mr-4">Game</Link>
+              <Link to="/register">Register</Link>
+            </div>
+          </div>
+        </nav>
+        
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<GamePage />} />
+          <Route path="/register" element={<RegistrationForm />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
